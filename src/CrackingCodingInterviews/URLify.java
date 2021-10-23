7e5bdeae8eabd9a3d53bd578d4dc0dbd305e1f8b
Imran Mohd Raji <imran.mohdraji@gmail.com>
2021-10-23 18:35:03 -0400

    Urlify Problem

--- src/CrackingCodingInterviews/URLify.java
+++ src/CrackingCodingInterviews/URLify.java
@@ -1,10 +1,24 @@
 package CrackingCodingInterviews;
 
 public class URLify {
-
+public static String urlify(String string,int length) {
+	string = string.substring(0,length);
+	System.out.println(string);
+	String strlst[] = string.split(" ");
+	System.out.println([1],strlst);
+	
+	
+	
+	return "hi";
+}
+	//Write a method to replace all spaces in a string with '%20'. Assume that the string has sufficient space at the end to hold the additional characters,
+	//and that you are given the true length of the string
 public static void main(String[] args) {
 // TODO Auto-generated method stub
-
+	String check ="Mr John Smith    ";
+	System.out.println(check);
+	int length = 13;
+	System.out.println(urlify(check, length));
 }
 
 }
